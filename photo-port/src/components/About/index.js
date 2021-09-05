0dd35e87af0dea6036893fb60e943b16c10c7cfb
imjord <dean.jay.goat@gmail.com>
2021-09-05 14:56:10 -0600

    completed about and nav components

---
+++ photo-port/src/components/About/index.js
@@ -0,0 +1,20 @@
+
+
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+import React from 'react';
+
+
+function About() {
+    return (
+        <section className="my-5">
+          
+            <h1 id="about"> Who am i? </h1>
+            <img src={coverImage} className="my-2" style={{width: "100%"}} alt="cover image"></img>
+            
+        </section>
+    );
+     
+}
+
+export default About;
